8ef8cfd1cae62d9a044342d39416ede64b04d831
reksher <stanislav-sannicov@mail.ru>
2023-10-04 20:04:58 +0400

    refactor test_items for lesson 3.6

--- test_items.py
+++ test_items.py
@@ -1,8 +1,13 @@
 import time
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
 def test_basket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-    time.sleep(5)
+    try:
+        basket = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+        assert basket is not None
+    except NoSuchElementException:
+        assert False, "Basket button not found"
+    time.sleep(30)
